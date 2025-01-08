5b6abb4c4cc62c1205ab3b6ccaec890926ea9486
Thapelo <tapsmcgzee8@gmail.com>
2025-01-08 18:07:01 +0200

    Provide the initial project structure for backend and frontend

---
+++ tourism-backend/src/test/java/com/tourism/tourism_backend/TourismBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tourism.tourism_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TourismBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
